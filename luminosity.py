3332d51a1e0ad283330df17b443b809e9e3d7da9
almasudme <almasudme@gmail.com>
2018-01-24 14:11:49 -0500

    Update luminosity.py

--- luminosity.py
+++ luminosity.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df= pd.read_table('luminosity.txt', sep = ',')
+
+plt.plot(df['Time'],df['Intensity'],marker='o')
+plt.xlabel('Clock Time (Hr)')
+plt.ylabel('Luminous Intensity micromol.m^-2.s^-1')
+plt.suptitle('Luminous intensity vs Time of day')
+plt.savefig('luminosity.jpg')
+plt.show()
 
